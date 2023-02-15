52d18345d15ddbdcbb623effc72914091a3fd415
Amirzhan <albert.git1@gmail.com>
2023-02-15 21:02:46 +0500

    microfix

--- apps/loader-module/api/index.js
+++ apps/loader-module/api/index.js
@@ -4,17 +4,18 @@ const multer = require('multer');
 const XLSX = require('xlsx');
 const extractData = require('../libs/data-extractor');
 
-const conf = require('./conf')
 const router = express.Router();
 
 router.use(express.json())
 router.use(multer({storage: multer.memoryStorage()}).single('file'));
 
 router.post('/', (req, res) => {
+    // TODO: валидировать входные данные
     const instructions = eval(req.body.instructions);
     const file = req.file;
+
     const workbook = XLSX.read(file.buffer);
-    console.log(workbook.SheetNames);
+
     res.send(extractData(instructions, workbook));
 });
 
